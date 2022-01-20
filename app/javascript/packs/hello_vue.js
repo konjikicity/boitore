44425d62bd9be3d85c6775a2d38923e7f47133e0
Syu <konjikicity@gmail.com>
2022-01-20 23:23:09 +0900

    chore: Vuetifyの導入 #6

--- app/javascript/packs/hello_vue.js
+++ app/javascript/packs/hello_vue.js
@@ -7,9 +7,15 @@
 
 import Vue from 'vue';
 import App from '../app.vue';
+import Vuetify from "vuetify";
+import "vuetify/dist/vuetify.min.css";
+
+Vue.use(Vuetify);
+const vuetify = new Vuetify();
 
 document.addEventListener('DOMContentLoaded', () => {
   const app = new Vue({
+    vuetify,
     render: h => h(App)
   }).$mount();
   document.body.appendChild(app.$el);
