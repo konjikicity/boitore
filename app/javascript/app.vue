da2821f6691aa4f0173f2c7f38ad17242957c17b
Syu <konjikicity@gmail.com>
2022-01-20 23:23:09 +0900

    [Add]Vuetifyの導入(#6)

--- app/javascript/app.vue
+++ app/javascript/app.vue
@@ -1,7 +1,17 @@
 <template>
-  <div id="app">
-    <p>{{ message }}</p>
-  </div>
+  <v-bottom-navigation v-model="value">
+    <v-btn value="recent">
+      <span>Recent</span>
+    </v-btn>
+ 
+    <v-btn value="favorites">
+      <span>Favorites</span>      
+    </v-btn>
+ 
+    <v-btn value="nearby">
+      <span>Nearby</span>      
+    </v-btn>
+  </v-bottom-navigation>
 </template>
 
 <script>
