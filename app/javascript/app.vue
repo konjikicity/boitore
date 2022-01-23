b53a6aaaf005f904ecdacfe774c4ff34a8e14bb5
Syu <konjikicity@gmail.com>
2022-01-23 15:04:18 +0900

    [Improve]yarn run lint-fix を実行(#6)

--- app/javascript/app.vue
+++ app/javascript/app.vue
@@ -1,65 +1,70 @@
 <template>
-<v-app>
-  <v-app-bar
-  color ="grey darken-3"
-  style= "height: 70px"
-   app>
-   <v-img 
-  max-height="200"
-  max-width="200"
-   src="../assets/boitore.png">
-   </v-img>
-  </v-app-bar>
-  <v-main>
-    <v-container fluid fill-height>
-   <v-row justify="center">
-      <v-col cols="auto">
-        <v-img 
-  max-height="500"
-  max-width="500"
-   src="../assets/top.png"></v-img>     
-        </v-col>
-    </v-row>
-    <v-row justify="center">
-        <v-btn
-              color="error"
-              dark
-              large
-            >
-              はじめてみる！
-            </v-btn>
-      </v-row>
-    </v-container>
-  </v-main>
-
- <v-footer
-    color="grey darken-3"
-    padless
-  >
-    <v-row
-      justify="center"
-      no-gutters
+  <v-app>
+    <v-app-bar
+      color="grey darken-3"
+      style="height: 70px"
+      app
     >
-      <v-btn
-        v-for="link in links"
-        :key="link"
-        color="white"
-        text
-        rounded
-        class="my-2"
-        style="text-transform: none"
+      <v-img 
+        max-height="200"
+        max-width="200"
+        src="../assets/boitore.png"
+      />
+    </v-app-bar>
+    <v-main>
+      <v-container
+        fluid
+        fill-height
       >
-        {{ link }}
-      </v-btn>
-      <v-col
-        class="grey darken-4 py-4 text-center white--text"
-        cols="12"
+        <v-row justify="center">
+          <v-col cols="auto">
+            <v-img 
+              max-height="500"
+              max-width="500"
+              src="../assets/top.png"
+            />     
+          </v-col>
+        </v-row>
+        <v-row justify="center">
+          <v-btn
+            color="error"
+            dark
+            large
+          >
+            はじめてみる！
+          </v-btn>
+        </v-row>
+      </v-container>
+    </v-main>
+
+    <v-footer
+      color="grey darken-3"
+      padless
+    >
+      <v-row
+        justify="center"
+        no-gutters
       >
-      <strong>©︎2021 boiトレ</strong>
-      </v-col>
-    </v-row>
-  </v-footer>
-</v-app>
+        <v-btn
+          v-for="link in links"
+          :key="link"
+          color="white"
+          text
+          rounded
+          class="my-2"
+          style="text-transform: none"
+        >
+          {{ link }}
+        </v-btn>
+        <v-col
+          class="grey darken-4 py-4 text-center white--text"
+          cols="12"
+        >
+          <strong>©︎2021 boiトレ</strong>
+        </v-col>
+      </v-row>
+    </v-footer>
+  </v-app>
 </template>
 
 <script>
