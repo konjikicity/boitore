986d62d001fac644d2a8c4428776af6772a27140
Syu <konjikicity@gmail.com>
2022-01-24 19:54:21 +0900

    [Improve]Header,Footerの単一コンポーネント化(#7)

--- app/javascript/app.vue
+++ app/javascript/app.vue
@@ -1,5 +1,21 @@
 <template>
-  <div>
-    <router-view />
-  </div>
+  <v-app>
+    <TheHeader />
+    <v-main>
+      <router-view />
+    </v-main>
+    <TheFooter />
+  </v-app>
 </template>
+
+<script>
+import TheHeader from "components/layout/TheHeader"
+import TheFooter from "components/layout/TheFooter"
+
+export default {
+  components: {
+    TheHeader,
+    TheFooter,
+  }
+}
+</script>
---
+++ app/javascript/components/layout/TheFooter.vue
@@ -0,0 +1,51 @@
+<template>
+  <v-footer
+    color="grey darken-3"
+    padless
+  >
+    <v-row
+      justify="center"
+      no-gutters
+    >
+      <v-btn
+        v-for="link in links"
+        :key="link"
+        color="white"
+        text
+        rounded
+        class="my-2"
+        style="text-transform: none"
+      >
+        {{ link }}
+      </v-btn>
+      <v-col
+        class="grey darken-4 py-4 text-center white--text"
+        cols="12"
+      >
+        <strong>{{ copylight }}</strong>
+      </v-col>
+    </v-row>
+  </v-footer>
+</template>
+
+ <script>
+  export default {
+    name: "TheFooter",
+    data: function () {
+    return {
+      copylight:"©︎2021 boiトレ",
+      links: [
+        'home',
+        'boiトレとは？',
+        '利用規約',
+        'プライバシーポリシー',
+        'お問い合わせ',
+      ]
+    }
+  }
+}
+ </script>
+
+  <style scoped>
+  
+  </style>
