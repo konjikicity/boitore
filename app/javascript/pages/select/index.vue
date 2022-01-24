49072a9d45424c98510e6930540877dcdae3ec2b
Syu <konjikicity@gmail.com>
2022-01-24 22:24:12 +0900

    [Add](仮)文字選択画面の作成(#7)

---
+++ app/javascript/pages/select/index.vue
@@ -0,0 +1,73 @@
+<template>
+  <v-container
+    fluid
+    fill-height
+  >
+  <div class="text-h3">
+  <p>文字の選択</p>
+  </div>
+  <v-carousel
+    cycle
+    height="500"
+    hide-delimiter-background
+    show-arrows-on-hover
+    hide-delimiters
+    class="rounded"
+  >
+    <template v-slot:prev="{ on, attrs }">
+      <v-btn
+        color="dark"
+        v-bind="attrs"
+        v-on="on"
+      >Previous slide</v-btn>
+    </template>
+    <template v-slot:next="{ on, attrs }">
+      <v-btn
+        color="dark"
+        v-bind="attrs"
+        v-on="on"
+      >Next slide</v-btn>
+    </template>
+    <v-carousel-item
+      v-for="(slide, i) in slides"
+      :key="i"
+    >
+      <v-sheet
+        color="teal lighten-5"
+        height="100%"
+      >
+        <v-row
+          class="fill-height"
+          align="center"
+          justify="center"
+        >
+          <div class="text-h2">
+            {{ slide }} Slide
+          </div>
+        </v-row>
+      </v-sheet>
+    </v-carousel-item>
+  </v-carousel>
+  </v-container>
+</template>
+
+<script>
+export default {
+  name: "SelectIndex",
+  data () {
+      return {
+        slides: [
+          'First',
+          'Second',
+          'Third',
+          'Fourth',
+          'Fifth',
+        ],
+      }
+  }
+}
+</script>
+
+<style scoped>
+
+</style>
