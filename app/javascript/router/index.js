a68e34f2ec29a8df9086eec28638d60a43b0d1ec
Syu <konjikicity@gmail.com>
2022-01-25 20:09:54 +0900

    refactor: router/index.jsの記述の修正 #7

--- app/javascript/router/index.js
+++ app/javascript/router/index.js
@@ -1,8 +1,5 @@
 // Load all the channels within this directory and all subdirectories.
 // Channel files must be named *_channel.js.
-
-const channels = require.context('.', true, /_channel\.js$/);
-channels.keys().forEach(channels);
 import Vue from "vue";
 import Router from "vue-router";
 
