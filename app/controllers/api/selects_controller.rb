87da6f1137a91517a7fe1a11eb0f66d2373612ef
Syu <konjikicity@gmail.com>
2022-01-25 21:11:45 +0900

    feat: jsonで取得した通常の文章(Sentence.normal)を文字選択画面に表示 #7

--- app/controllers/api/selects_controller.rb
+++ app/controllers/api/selects_controller.rb
@@ -1,6 +1,6 @@
 class Api::SelectsController < ApplicationController
   def index
-    normal = Sentence.select(:id,:normal)
+    normal = Sentence.select(:id, :normal)
     render json: normal
   end
 end
