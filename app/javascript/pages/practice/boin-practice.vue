61ba0181e20bfc3277805ffe0ffa84517c3e2b3f
Syu <konjikicity@gmail.com>
2022-01-29 20:52:47 +0900

    feat: 母音法の練習ページの作成 #8

--- app/javascript/pages/practice/boin-practice.vue
+++ app/javascript/pages/practice/boin-practice.vue
@@ -1,12 +1,66 @@
 <template>
-  <div class="product">
-    <h1>母音練習</h1>
-    <h2>{{ $route.params.normal }}</h2>
-    <h2>{{ $route.params.boin }}</h2>
-  </div>
+  <v-container 
+    fill-height 
+    fluid
+  >
+    <v-row
+      justify="center"
+      align-content="center"
+    >
+      <v-col
+        cols="9"
+      >
+        <div>
+          <h1
+            class="mb-16"
+          >
+            母音法で練習
+          </h1>
+        </div>
+        <v-card 
+          height="200"
+          class="mb-3"
+          elevation="20"
+        >
+          <v-card-title>
+            <h3>母音の文章</h3>
+          </v-card-title>
+          <v-card-text>
+            <div
+              class="text-h2 text-center align-items-center"
+            >
+              {{ $route.params.boin }}
+            </div>
+          </v-card-text>
+        </v-card>
+        <v-card 
+          height="200"
+          class="mb-16"
+          elevation="20"
+        >
+          <v-card-title>
+            <h3>通常の文章</h3>
+          </v-card-title>
+          <v-card-text>
+            <div
+              class="text-h2 text-center align-items-center"
+            >
+              {{ $route.params.normal }}
+            </div>
+          </v-card-text>
+        </v-card>
+        <p
+          class="text-center"
+        >
+          結果画面へ
+        </p>     
+      </v-col>
+    </v-row>
+  </v-container>
 </template>
 <script>
 export default {
- props: ["id"]
+ 
+
 }
 </script>
