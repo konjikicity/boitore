3b318165e50318c8009bb5f2b47a6bc64447ca9e
Syu <konjikicity@gmail.com>
2022-02-01 21:38:42 +0900

    [Add]モーダル内のレイアウトを作成(#10)

--- app/javascript/components/result/PracticeResult.vue
+++ app/javascript/components/result/PracticeResult.vue
@@ -1,14 +1,64 @@
 <template>
+  <v-row justify="space-around">
+    <v-col cols="auto">
       <v-dialog
         transition="dialog-bottom-transition"
-        max-width="600"
+        max-width="auto"
       >
         <template v-slot:activator="{ on, attrs }">
           <v-btn
             color="error"
             v-bind="attrs"
             v-on="on"
-          >結果を見る！</v-btn>
+          >
+            結果を見る!
+          </v-btn>
+        </template>
+
+        <template v-slot:default="dialog">
+          <v-card>
+            <div class="text-h3 text-center py-9">
+              録音結果！
+            </div>
+            <v-divider />
+            <v-row
+              justify="center"
+            >
+              <v-col
+                class="text-center py-9"
+                cols="5"
+              >
+                <p>通常の文章</p>
+                <audio controls />
+              </v-col>
+              <v-col
+                class="text-center py-9"
+                cols="5"
+              >
+                <p>母音の文章</p>
+                <audio controls />
+              </v-col>
+            </v-row>
+            <v-divider />
+            <v-card-actions class="justify-center">      
+              <v-btn
+                text
+                :to="{ name: 'SelectIndex'}"
+                @click="dialog = false"
+              >
+                もう一度練習する
+              </v-btn>
+              <v-btn
+                text
+                :to="{ name: 'TopIndex'}"
+                @click="dialog = false"
+              >
+                タイトルへ戻る
+              </v-btn>
+            </v-card-actions>
+          </v-card>
         </template>
       </v-dialog>
+    </v-col>
+  </v-row>
 </template>
--- app/javascript/pages/practice/boin-practice.vue
+++ app/javascript/pages/practice/boin-practice.vue
@@ -24,11 +24,12 @@
           <v-card-title>
             <h3>母音の文章</h3>
           </v-card-title>
-            <div
-              class="text-h2 text-center py"
-            >
-              {{ $route.params.boin }}
-            </div>
+          <v-divider />
+          <div
+            class="text-h2 text-center py-14"
+          >
+            {{ $route.params.boin }}
+          </div>
         </v-card>
         <v-card
           class="mb-16"
@@ -37,15 +38,17 @@
           <v-card-title>
             <h3>通常の文章</h3>
           </v-card-title>
-            <div
-              class="text-h2 text-center py"
-            >
-              {{ $route.params.normal }}
-            </div>
+          <v-divider/>
+          <div
+            class="text-h2 text-center py-14"
+          >
+            {{ $route.params.normal }}
+          </div>
         </v-card>
         <div
-        class="text-center">
-      <PracticeResult />
+          class="text-center"
+        >
+          <PracticeResult />
         </div>
       </v-col>
     </v-row>
@@ -63,10 +66,5 @@ export default {
 }
 </script>
 <style scoped>
-.v-card__title {
-  border-bottom: 1px solid #eee;
-}
-.py {
-  padding: 60px 0;
-}
+
 </style>
