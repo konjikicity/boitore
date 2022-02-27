4204ebb565aba57663d58b5de14d4f0094adee8c
Syu <konjikicity@gmail.com>
2022-02-28 01:45:43 +0900

    [Fix](仮)でaxiosのbaseurlを設定(#28)

--- app/javascript/plugins/axios.js
+++ app/javascript/plugins/axios.js
@@ -1,7 +1,7 @@
 import axios from 'axios'
 
 const axiosInstance = axios.create({
-  baseURL: 'http://localhost:3000/api/'
+  baseURL: 'https://boitore.herokuapp.com/api/'
 })
 
 export default axiosInstance
