e613044c93e2745cc94f1fdb8ae60396a3d0d483
Syu <konjikicity@gmail.com>
2022-03-11 23:00:41 +0900

    [add]文章の追加(#82)

--- db/fixtures/development/sentence.rb
+++ db/fixtures/development/sentence.rb
@@ -13,13 +13,49 @@ Sentence.seed do |s|
 end
 Sentence.seed do |s|
   s.id = 2
-  s.boin =  "おあおうおあいあう"
+  s.boin = "おんあんあ"
+  s.normal = "こんばんわ"
+  s.mode_id = 1
+end
+Sentence.seed do |s|
+  s.id = 3
+  s.boin =  "おあおう おあいあう"
   s.normal= "おはようございます"
   s.mode_id= 1
 end
 Sentence.seed do |s|
-  s.id = 3
-  s.boin = "おおいうおえあいいあう"
-  s.normal= "よろしくおねがいします"
-  s.mode_id= 1
+  s.id = 4
+  s.boin = "えんあうおお おいおんえ"
+  s.normal = "せんたくものとりこんで"
+  s.mode_id= 2
+end
+Sentence.seed do |s|
+  s.id = 5
+  s.boin = "おおおお いあああ"
+  s.normal = "そろそろいかなきゃ"
+  s.mode_id = 2
+end
+Sentence.seed do |s|
+  s.id = 6
+  s.boin = "ああいあ えんいいあ えう"
+  s.normal = "わたしはえんじにあです"
+  s.mode_id = 2
+end
+Sentence.seed do |s|
+  s.id = 7
+  s.boin = "ああうう えっううう"
+  s.normal = "まさちゅーせっつしゅう"
+  s.mode_id = 3
+end
+Sentence.seed do |s|
+  s.id = 8
+  s.boin = "いおんおうおう いううあ いえん"
+  s.normal = "きほんじょうほうぎじゅつしゃしけん"
+  s.mode_id = 3
+end
+Sentence.seed do |s|
+  s.id = 9
+  s.boin = "いんえう いんあう いういあう"
+  s.normal = "しんせつしんさつしつしさつ"
+  s.mode_id = 3
 end
