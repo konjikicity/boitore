6ac0332a63860085078c733ef4259bb92804c9e0
Syu <konjikicity@gmail.com>
2022-03-14 07:57:41 +0900

    [add]modeコンポーネントの作成(#82)

---
+++ app/javascript/pages/mode/index.vue
@@ -0,0 +1,39 @@
+<template>
+  <v-container
+  fill-height
+  fluid
+  >
+  <v-row>
+  <v-btn
+  v-for="(mode, index) of modes"
+    :key="mode"
+  >
+  {{ mode }}
+  </v-btn>
+  </v-row>
+  </v-container>
+</template>
+<script>
+export default {
+  name: "ModeIndex",
+  data(){
+    return {
+      modes: []
+    }
+  },
+  mounted() {
+    this.fetchModes();
+  },
+  methods: {
+    fetchModes() {
+      this.$axios.get('modes')
+
+
+        .then(res => this.modes = res.data)
+        .catch(err => console.log(err.status));
+    }
+  }
+}
+</script>
+<style scoped>
+</style>
