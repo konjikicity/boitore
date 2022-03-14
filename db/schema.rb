87e66ba68bcfb10caae387bed5670ee850670bdf
Syu <konjikicity@gmail.com>
2022-03-15 00:22:40 +0900

    feat: modeコンポーネントの作成 #82

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2022_03_11_121514) do
+ActiveRecord::Schema.define(version: 2022_03_14_143524) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -19,6 +19,7 @@ ActiveRecord::Schema.define(version: 2022_03_11_121514) do
     t.string "difficulty", null: false
     t.datetime "created_at", precision: 6, null: false
     t.datetime "updated_at", precision: 6, null: false
+    t.string "description"
   end
 
   create_table "sentences", force: :cascade do |t|
--- spec/factories/modes.rb
+++ spec/factories/modes.rb
@@ -2,10 +2,11 @@
 #
 # Table name: modes
 #
-#  id         :bigint           not null, primary key
-#  difficulty :string           not null
-#  created_at :datetime         not null
-#  updated_at :datetime         not null
+#  id          :bigint           not null, primary key
+#  description :string
+#  difficulty  :string           not null
+#  created_at  :datetime         not null
+#  updated_at  :datetime         not null
 #
 FactoryBot.define do
   factory :mode do
