d211e34ba2635629d65ed86daea10437180e3cf0
Syu <konjikicity@gmail.com>
2022-03-23 21:33:12 +0900

    refactor: modesテーブルのdifficultyカラムにenumを使用 #93

--- spec/factories/modes.rb
+++ spec/factories/modes.rb
@@ -4,7 +4,7 @@
 #
 #  id          :bigint           not null, primary key
 #  description :string
-#  difficulty  :string           not null
+#  difficulty  :integer          not null
 #  created_at  :datetime         not null
 #  updated_at  :datetime         not null
 #
