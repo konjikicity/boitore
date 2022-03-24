178f03c63d2199c7b5fdfec64917014d081c4c55
Syu <konjikicity@gmail.com>
2022-03-25 00:18:20 +0900

    fix: deproi時modesが表示されない部分を追加修正 #96

--- spec/models/mode_spec.rb
+++ spec/models/mode_spec.rb
@@ -4,7 +4,7 @@
 #
 #  id          :bigint           not null, primary key
 #  description :string
-#  difficulty  :integer          not null
+#  difficulty  :string           not null
 #  created_at  :datetime         not null
 #  updated_at  :datetime         not null
 #
