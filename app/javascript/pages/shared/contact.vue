77f4a19bcc6c9b152f76c1aa922e3d48620d18bf
Syu <konjikicity@gmail.com>
2022-03-26 10:31:15 +0900

    feat: お問合せの追加 #101

---
+++ app/javascript/pages/shared/contact.vue
@@ -0,0 +1,41 @@
+<template>
+  <v-container class="inquiry white--text" fill-height fluid>
+    <v-row align-content="center">
+      <v-spacer />
+      <v-col class="text-center" cols="10">
+        <p>サービスの利用に関するお問い合わせ、不具合の報告などがございましたら、</p>
+        <p>お手数ですが、以下のTwitterアカウント宛のダイレクトメールにてお知らせください。</p>
+        <v-icon>mdi-email</v-icon>
+        <p>
+          <v-btn
+            href="https://twitter.com/konjikicity"
+            min-height="20"
+            class="x-small light-blue white--text"
+            rounded
+            style="text-transform: none"
+          >
+          <v-icon
+          class="mr-2"
+          >
+          mdi-twitter
+          </v-icon>
+          @konjikicity
+          </v-btn>
+        </p>
+      </v-col>
+      <v-spacer />
+    </v-row>
+  </v-container>
+</template>
+<style scoped>
+.post-link {
+  display: inline-block;
+  word-break: break-all;
+  padding: 2px 8px;
+  border-radius: 5px;
+  text-transform: none !important;
+  white-space: normal;
+  max-width: calc(100% - 30px);
+  height: inherit !important;
+}
+</style>
