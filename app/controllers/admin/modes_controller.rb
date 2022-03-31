5431da6a7ab04a6231a89cbbab8ffa76d458d660
Syu <konjikicity@gmail.com>
2022-04-01 01:23:09 +0900

    feat: 管理画面のcontrollerを作成 #103

--- app/controllers/admin/modes_controller.rb
+++ app/controllers/admin/modes_controller.rb
@@ -1,2 +1,35 @@
 class Admin::ModesController < Admin::BaseController
+  before_action :set_mode, only: %i[edit update show destroy]
+  
+  def index
+    @modes = Mode.all.includes(:mode).order(id: :asc)
+  end
+
+  def edit; end
+
+  def update
+    if @mode.update(mode_params)
+      redirect_to admin_board_path(@mode), success: t('defaults.message.updated', item: Mode.model_name.human)
+    else
+      flash.now['danger'] = t('defaults.message.not_updated', item: Mode.model_name.human)
+      render :edit
+    end
+  end
+
+  def show; end
+
+  def destroy
+    @board.destroy!
+    redirect_to admin_modes_path, success: t('defaults.message.deleted', item: Mode.model_name.human)
+  end
+
+  private
+
+  def set_mode
+    @mode = Mode.find(params[:id])
+  end
+
+  def mode_params
+    params.require(:mode).permit(:dificullty, :description)
+  end
 end
--- app/controllers/admin/sentences_controller.rb
+++ app/controllers/admin/sentences_controller.rb
@@ -1,2 +1,35 @@
 class Admin::SentencesController < Admin::BaseController
+  before_action :set_sentence, only: %i[edit update show destroy]
+  
+  def index
+    @sentences = Mode.all.includes(:mode).order(id: :asc)
+  end
+
+  def edit; end
+
+  def update
+    if @sentence.update(sentence_params)
+      redirect_to admin_board_path(@sentence), success: t('defaults.message.updated', item: Sentence.model_name.human)
+    else
+      flash.now['danger'] = t('defaults.message.not_updated', item: Sentence.model_name.human)
+      render :edit
+    end
+  end
+
+  def show; end
+
+  def destroy
+    @sentence.destroy!
+    redirect_to admin_sentences_path, success: t('defaults.message.deleted', item: Sentence.model_name.human)
+  end
+
+  private
+
+  def set_sentence
+    @sentence = Sentence.find(params[:id])
+  end
+
+  def sentence_params
+    params.require(:sentence).permit(:boin, :normal, :mode_id)
+  end
 end
