943228151a825efa79713ba9b604e97f42caf088
Syu <konjikicity@gmail.com>
2022-04-01 20:47:22 +0900

    fix: タイポの修正 #103

--- app/controllers/admin/modes_controller.rb
+++ app/controllers/admin/modes_controller.rb
@@ -2,14 +2,14 @@ class Admin::ModesController < Admin::BaseController
   before_action :set_mode, only: %i[edit update show destroy]
   
   def index
-    @modes = Mode.all
+    @modes = Mode.all.order(:id)
   end
 
   def edit; end
 
   def update
     if @mode.update(mode_params)
-      redirect_to admin_board_path(@mode), success: t('defaults.message.updated', item: Mode.model_name.human)
+      redirect_to admin_mode_path(@mode), success: t('defaults.message.updated', item: Mode.model_name.human)
     else
       flash.now['danger'] = t('defaults.message.not_updated', item: Mode.model_name.human)
       render :edit
@@ -19,7 +19,7 @@ class Admin::ModesController < Admin::BaseController
   def show; end
 
   def destroy
-    @board.destroy!
+    @mode.destroy!
     redirect_to admin_modes_path, success: t('defaults.message.deleted', item: Mode.model_name.human)
   end
 
