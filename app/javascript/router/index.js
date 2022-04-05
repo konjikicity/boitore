4c9dc8b59b15803c2f45ad898b4bd12130eab6d2
Syu <konjikicity@gmail.com>
2022-04-05 13:39:46 +0900

    feat: (仮)でMyPageを作成 #11

--- app/javascript/router/index.js
+++ app/javascript/router/index.js
@@ -14,6 +14,7 @@ import SharedPrivacy from "../pages/shared/privacy-policy";
 import SharedContact from "../pages/shared/contact";
 import LoginForm from "../pages/login/login-form";
 import SignUpForm from "../pages/login/signup-form";
+import MyPageIndex from "../pages/MyPage/index";
 
 Vue.use(Router)
 
@@ -77,9 +78,13 @@ const router = new Router({
       path: '/signup',
       component: SignUpForm,
       name: "SignUpForm"
+    },
+    {
+      path:'/MyPage',
+      component: MyPageIndex,
+      name: "MyPageIndex"
     }
-   
-  ],
+  ]
 })
 
 export default router
