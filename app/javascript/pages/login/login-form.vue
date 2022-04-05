0c67f1a3aed8f2e69c50dcfc120c2756aa34363d
Syu <konjikicity@gmail.com>
2022-04-05 12:52:22 +0900

    feat: ユーザーログインのmethodsを追加 #11

--- app/javascript/pages/login/login-form.vue
+++ app/javascript/pages/login/login-form.vue
@@ -6,7 +6,9 @@
     <v-card-title>
       <h1 class="display-1">ログイン</h1>
     </v-card-title>
-    <v-form>
+    <v-form
+    @submit.prevent="login"
+    >
 <v-text-field 
 prepend-icon="mdi-account-circle"
 label="メールアドレス"
@@ -18,19 +20,22 @@ v-bind:type="showPassword ? 'text' : 'password'"
 prepend-icon="mdi-lock" 
 v-bind:append-icon="showPassword ? 'mdi-eye' : 'mdi-eye-off'" 
 label="パスワード" 
+v-model="password"
 @click:append="showPassword = !showPassword" 
 class="px-7"
 />
   <v-card-actions>
     <v-btn 
     class="error ml-7"
-    @click="submit"
+    @click="login"
     >ログイン</v-btn>
   </v-card-actions>
 </v-form>
   </v-card>
 </template>
 <script>
+import axios from 'axios'
+
 export default {
   name: "LoginForm",
   data(){
@@ -41,8 +46,20 @@ export default {
     }
   },
   methods: {
-    submit(){
+    async login(){
+      try {
 
+        const res = await axios.post('http://localhost:3000/auth/sign_in', {
+          email: this.email,
+          password: this.password,
+          }
+        )
+
+        console.log({ res })
+        return res
+      } catch (error) {
+        console.log({ error })
+      }
     }
   }
 }
