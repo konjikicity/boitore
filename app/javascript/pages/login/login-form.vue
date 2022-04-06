baa5e95063519b7d0f7c6ec6675a4d6aa88a3197
Syu <konjikicity@gmail.com>
2022-04-06 22:03:21 +0900

    refactor: yarn run lint-fixを実行 #11

--- app/javascript/pages/login/login-form.vue
+++ app/javascript/pages/login/login-form.vue
@@ -1,36 +1,40 @@
 <template>
   <v-card
-  width="800px"
-  class="mx-auto mt-5"
+    width="800px"
+    class="mx-auto mt-5"
   >
     <v-card-title>
-      <h1 class="display-1">ログイン</h1>
+      <h1 class="display-1">
+        ログイン
+      </h1>
     </v-card-title>
     <v-form
-    @submit.prevent="login"
+      @submit.prevent="login"
     >
-<v-text-field 
-prepend-icon="mdi-account-circle"
-label="メールアドレス"
-v-model="email"
-class="px-7"  
-/>
-<v-text-field 
-v-bind:type="showPassword ? 'text' : 'password'" 
-prepend-icon="mdi-lock" 
-v-bind:append-icon="showPassword ? 'mdi-eye' : 'mdi-eye-off'" 
-label="パスワード" 
-v-model="password"
-@click:append="showPassword = !showPassword" 
-class="px-7"
-/>
-  <v-card-actions>
-    <v-btn 
-    class="error ml-7"
-    @click="login"
-    >ログイン</v-btn>
-  </v-card-actions>
-</v-form>
+      <v-text-field 
+        v-model="email"
+        prepend-icon="mdi-account-circle"
+        label="メールアドレス"
+        class="px-7"  
+      />
+      <v-text-field 
+        v-model="password" 
+        :type="showPassword ? 'text' : 'password'" 
+        prepend-icon="mdi-lock" 
+        :append-icon="showPassword ? 'mdi-eye' : 'mdi-eye-off'" 
+        label="パスワード"
+        class="px-7" 
+        @click:append="showPassword = !showPassword"
+      />
+      <v-card-actions>
+        <v-btn 
+          class="error ml-7"
+          @click="login"
+        >
+          ログイン
+        </v-btn>
+      </v-card-actions>
+    </v-form>
   </v-card>
 </template>
 <script>
@@ -52,7 +56,7 @@ export default {
         const res = await axios.post('http://localhost:3000/auth/sign_in', {
           email: this.email,
           password: this.password,
-          }
+        }
         )
         setItem(res.headers, res.data.data.name)
         this.$store.commit('updateToken', res.headers["access-token"])
