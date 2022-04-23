11a09ccf0e24b471e4340684690106906d3b4d2c
Syu <konjikicity@gmail.com>
2022-04-23 23:25:35 +0900

    style: カレンダーの表示を大きくしたいためfooterを削除 #126

--- app/javascript/pages/my-page/index.vue
+++ app/javascript/pages/my-page/index.vue
@@ -1,7 +1,7 @@
 <template>
   <v-container>
     <v-row>
-      <v-col cols="2">
+      <v-col cols="3">
         <v-sheet rounded="lg">
           <v-list color="transparent">
             <v-list-item
@@ -39,7 +39,7 @@
 
       <v-col>
         <v-sheet
-          height="73vh"
+          height="70vh"
           rounded="lg"
         >
           <Calendar />
