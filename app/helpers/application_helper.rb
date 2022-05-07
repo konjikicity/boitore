a6d2c860a23b90a008061f68074d581e0a784b53
Syu <konjikicity@gmail.com>
2022-05-07 11:43:23 +0900

    style: meta_tagsの設定を変更 #172

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -2,16 +2,20 @@ module ApplicationHelper
   def default_meta_tags
     {
       site: 'BOIトレ',
-      title: 'BOIトレ ~母音法で発声練習~',
+      title: '~母音法で発声練習~',
+      reverse: true,
+      charset: 'utf-8',
       description: 'BOIトレは劇団四季も使っている母音法で練習する、発声練習・滑舌改善サービスです。',
-      canonical: request.original_url,   # 優先するurlを指定する
-      noindex: !Rails.env.production?,
-      charset: 'UTF-8',
+      canonical: request.original_url,
+      separator: '|',
+      icon: [
+        { href: image_url('favicon.ico') },
+      ],
       og: {
+        site_name: :site,
         title: :title,
         description: :description,
         type: 'website',
-        site_name: :site,
         url: request.original_url,
         image: image_url('boitore_ogp.png'),
         locale: 'ja_JP'
