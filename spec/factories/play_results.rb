fc8681f5d43bfeeaed83aee7272a7c6dc045d2c6
Syu <konjikicity@gmail.com>
2022-05-11 20:55:31 +0900

    feat: play_resultスペックを作成 #190

--- spec/factories/play_results.rb
+++ spec/factories/play_results.rb
@@ -24,6 +24,12 @@
 #
 FactoryBot.define do
   factory :play_result do
-    
+    boin_voice { [ Rack::Test::UploadedFile.new(Rails.root.join('spec/fixtures/loop100302.wav'), 'spec/fixtures/loop100302.wav') ] }
+    normal_voice  { [ Rack::Test::UploadedFile.new(Rails.root.join('spec/fixtures/loop100302.wav'), 'spec/fixtures/loop100302.wav') ] }
+    judge { "S" }
+    practiced_boin { "こんにちは" }
+    practiced_normal { "こんにちは" }
+    practiced_sentence { "こんにちは" }
+    score { "67" }
   end
 end
