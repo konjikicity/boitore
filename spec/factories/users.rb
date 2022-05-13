e07bafb62b646f620f4019290ac2b209922d9f09
Syu <konjikicity@gmail.com>
2022-05-13 11:06:41 +0900

    feat: user_specの作成 #203

---
+++ spec/factories/users.rb
@@ -0,0 +1,7 @@
+FactoryBot.define do
+    factory :user do
+      name { Faker::Name.name }
+      sequence(:email) { |n| "#{n}_" + Faker::Internet.email }
+      password { Faker::Internet.password(min_length:8) }
+    end
+end
---
+++ spec/models/user_spec.rb
@@ -0,0 +1,43 @@
+require 'rails_helper'
+
+RSpec.describe User, type: :model do
+  describe "必須項目のバリデーション確認" do
+    context "全カラムの値を指定しているとき" do
+      let(:user) { create(:user) }
+
+      it "userのレコードが作成される" do
+        expect(user).to be_valid
+      end
+    end
+
+    context "emailを指定していないとき" do
+      let(:user) { build(:user, email: nil) }
+
+      it "エラーになる" do
+        user.valid?
+        expect(user.errors.messages[:email]).to include "を入力してください"
+      end
+    end
+
+    context "passwordを指定していないとき" do
+      let(:user) { build(:user, password: nil) }
+
+      it "エラーになる" do
+        user.valid?
+        expect(user.errors.messages[:password]).to include "を入力してください"
+      end
+    end
+  end
+
+  describe "重複のバリデーション確認" do
+    context "保存されたメールアドレスが指定されたとき" do
+      let(:user1) { create(:user) }
+      let(:user2) { build(:user, email: user1.email) }
+
+      it "エラーになる" do
+        user2.valid?
+        expect(user2.errors.messages[:email]).to include "はすでに存在します"
+      end
+    end
+  end
+end
