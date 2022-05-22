d2a834ee7f0b32b997f2b9901241c34d0f23358a
Syu <konjikicity@gmail.com>
2022-05-22 11:46:32 +0900

    fix: バリデーションを変更していたのでテストが通らなくなった部分を修正 #223

--- spec/support/driver_setting.rb
+++ spec/support/driver_setting.rb
@@ -1,7 +1,7 @@
 RSpec.configure do |config|
   config.before(:each, type: :system) do
     # Spec実行時、ブラウザが自動で立ち上がり挙動を確認できる
-    driven_by(:selenium_chrome)
+    # driven_by(:selenium_chrome)
 
     # Spec実行時、ブラウザOFF
     driven_by(:selenium_chrome_headless)
--- spec/support/sign_up_module.rb
+++ spec/support/sign_up_module.rb
@@ -5,6 +5,7 @@ module SignUpModule
     fill_in 'メールアドレス', with: user.email
     fill_in 'パスワード', with: 'password'
     fill_in 'パスワード確認', with: 'password'
+    send_keys :return
     click_on '登録'
   end
 end
