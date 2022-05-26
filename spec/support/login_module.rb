d587bec9610c4860085ba8b98dd4bc7fe77e35de
Syu <konjikicity@gmail.com>
2022-05-26 11:40:32 +0900

    feat: admin_login_specの作成 #232

--- spec/support/login_module.rb
+++ spec/support/login_module.rb
@@ -1,8 +1,8 @@
 module LoginModule
   def login(user)
     visit('/login')
-    fill_in 'Email', with: user.email
-    fill_in 'Password', with: 'password'
+    fill_in 'メールアドレス', with: user.email
+    fill_in 'パスワード', with: 'password'
     click_on 'ログイン'
   end
 end
