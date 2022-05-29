287407c2f7b2ea9ca3c57f6da290ea48e9f2fcea
Syu <konjikicity@gmail.com>
2022-05-30 08:56:46 +0900

    sentenceをjbを使用するものに変更 #234

--- app/controllers/api/v1/selects_controller.rb
+++ app/controllers/api/v1/selects_controller.rb
@@ -1,11 +1,11 @@
 class Api::V1::SelectsController < Api::V1::ApplicationController
   def index
-    sentences = Sentence.where(mode_id: params[:mode_id])
-    render json: sentences, each_serializer: SentenceSerializer
+    @sentences = Sentence.where(mode_id: params[:mode_id])
+    render "jb/sentence/index.json.jb"
   end
 
   def show
-    sentence = Sentence.find(params[:id])
-    render json: sentence, serializer: SentenceSerializer
+    @sentence = Sentence.find(params[:id])
+    render "jb/sentence/show.json.jb"
   end
 end
