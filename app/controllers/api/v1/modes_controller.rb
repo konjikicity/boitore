97cdfe0bc8e092774777e1f5bf7ed704f2336952
Syu <konjikicity@gmail.com>
2022-05-30 08:39:50 +0900

    refactor: modeをjbを使用するものに変更 #234

--- app/controllers/api/v1/modes_controller.rb
+++ app/controllers/api/v1/modes_controller.rb
@@ -1,6 +1,6 @@
 class Api::V1::ModesController < Api::V1::ApplicationController
   def index
-    modes = Mode.all.order(:id)
-    render json: modes, each_serializer: ModeSerializer
+    @modes = Mode.all.order(:id)
+    render "jb/mode.json.jb"
   end
 end
