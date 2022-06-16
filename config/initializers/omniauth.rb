9894a939a5c2b2da5488785c639fcda018f7dabd
Syu <konjikicity@gmail.com>
2022-06-17 03:13:26 +0900

    feat: google認証の設定をomiauth.rbに追加 #193

--- config/initializers/omniauth.rb
+++ config/initializers/omniauth.rb
@@ -1,5 +1,6 @@
 Rails.application.config.middleware.use OmniAuth::Builder do
   provider :twitter, ENV['TWITTER_API_KEY'], ENV['TWITTER_SECRET']
+  provider :google_oauth2, ENV['GOOGLE_API_KEY'], ENV['GOOGLE_SECRET'], name: :google, scope: %w(email)
   OmniAuth.config.allowed_request_methods = [:get, :post]
   OmniAuth.config.silence_get_warning = true
 end
