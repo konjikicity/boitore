7a98f4c768bb72241aabc7df631fef69f20201a8
Syu <konjikicity@gmail.com>
2022-06-21 02:51:58 +0900

    style: topページの画像サイズを変更 #261

--- app/javascript/pages/top/index.vue
+++ app/javascript/pages/top/index.vue
@@ -9,8 +9,8 @@
     >
       <v-img
         src="https://res.cloudinary.com/dzlhvpfmo/image/upload/v1651544233/boi%E3%83%88%E3%83%AC/top_eyvzeg.webp"
-        max-width="800"
-        max-height="500"
+        max-width="700"
+        max-height="380"
       />
     </v-row>
     <v-row
